d698a6dd7e7e084af44433d3d8b5555f4cb91d01
MaxZambada <47768331+MaxZambada@users.noreply.github.com>
2020-12-07 13:32:57 -0600

    Add files via upload

--- main.vhd
+++ main.vhd
@@ -45,7 +45,8 @@ entity main is
            LCD1 : out STD_LOGIC_VECTOR (7 downto 0);
            D71 : out STD_LOGIC_VECTOR (7 downto 0);
            T1, T2 : out STD_LOGIC_VECTOR (3 downto 0);
-           D7A : out STD_LOGIC_VECTOR (1 downto 0));
+           D7A : out STD_LOGIC_VECTOR (7 downto 0);
+           NumLED : out STD_LOGIC_VECTOR(4 downto 0));
 end main;
 
 architecture Behavioral of main is
@@ -73,22 +74,21 @@ PORT(CLK: IN STD_LOGIC;
 	  Int1, Int2 : IN integer);
 end component;
 
-component puntos is
-    Port ( Qout : in STD_LOGIC;
-           Boton : in integer;
-           TOut : in STD_LOGIC;
-           IN1: in integer range 0 to 9;
-           IN2: in integer range 0 to 9;
-           ON1: out integer range 0 to 9;
-           ON2: out integer range 0 to 9);
-end component;
+--component puntos is
+--    Port ( --Qout : in STD_LOGIC;
+--           B1, B2, B3 : in STD_LOGIC;
+--           TOut : in STD_LOGIC;
+--           IN1: in integer range 0 to 9;
+--           IN2: in integer range 0 to 9;
+--           ON1: out integer range 0 to 9;
+--           ON2: out integer range 0 to 9);
+--end component;
 
 component segm7 is
-Port(
-        Clk: in std_logic;
-        N1, N2: in std_logic_vector(3 downto 0);
-        S81, S82: out std_logic_vector(7 downto 0)
-);
+Port(   ck : in  std_logic;
+        number : in  std_logic_vector (63 downto 0); -- eight digit number to be displayed
+        seg : out  std_logic_vector (7 downto 0);    -- display cathodes
+		an : out  std_logic_vector (7 downto 0)); 
 end component;
 
 component clk_wiz_0
@@ -105,14 +105,14 @@ port
  );
 end component;
 
-component one_shot is
-    Port ( B1 : in STD_LOGIC;
-           B2 : in STD_LOGIC;
-           B3 : in STD_LOGIC;
-           clk : in STD_LOGIC;
-           Qout : out STD_LOGIC;
-           Boton : out integer);
-end component;
+--component one_shot is
+--    Port ( B1 : in STD_LOGIC;
+--           B2 : in STD_LOGIC;
+--           B3 : in STD_LOGIC;
+--           clk : in STD_LOGIC;
+--           --Qout : out STD_LOGIC;
+--           Boton : out integer);
+--end component;
 
 --SLock: Variable extra nadamas para el port map de reloj
 --STO : Bloquear botones para puntos
@@ -127,37 +127,39 @@ end component;
 --IntPuls, IntComp: Valores para generar señal PWM
 --IntTO: Tiempo que le resta al bloqueo de botones
 --IntTime: Contador para que se tenga 100ms y limitar el paso del tiempo
-signal STO : std_logic := '1';
+signal STO : std_logic := '0';
 signal SCLK100, SCLK1, SCLK50, SON, SLock, SLCDE, Qout: std_logic := '0';
-signal SMUX : STD_LOGIC_VECTOR(1 downto 0) := "01";
+signal SMUX : STD_LOGIC_VECTOR(7 downto 0) := "00000001";
 signal SP1, SP2, STJ1, STJ2 : std_logic_vector(3 downto 0):= "0000";
 signal STJ71, STJ72: std_logic_vector(7 downto 0)  := "00000000";
 signal Int1, Int2, Int3, Int4, Int5, IntTJ1, IntTJ2, IntPuls, IntComp, IntTime: integer := 0;
 signal IntTO, Boton: integer := 0;
+signal d7s : STD_LOGIC_VECTOR (63 downto 0) := "1111111111111111111111111111111111111111111111110000000000000000";
 
 begin
 --PPuntos: Calcula los puntos al oprimir cada boton
 --PSegm7: Convierte los valores de 4 bits a señal de 7 para los Displays
 --PClocks: Genera diferentes señales de Clocks
 --PLCD: Para utilizar el LCD
-PPuntos: puntos port map(Qout, Boton, STO, Int1, Int2, Int3, Int4);
-PSegm7: segm7 port map(Clk100MHZ, STJ1, STJ2, STJ71, STJ72);
+--PPuntos: puntos port map(B2, B3, B4, STO, Int1, Int2, Int3, Int4);
+PSegm7: segm7 port map(Clk100MHZ, d7s, D7A, D71);
 PClocks: clk_wiz_0 port map(SCLK100, SCLK1, SCLK50, '0', SLock, CLK100MHZ);
 PLCD: LIB_LCD_INTESC_REVD port map(Clk100MHZ, RS, RW, EN, LCD1, Int2, Int1);
-OneS: One_shot port map(B2, B3, B4, CLK100MHZ, Qout, Boton);
+--OneS: One_shot port map(B2, B3, B4, CLK100MHZ, Boton);
 
+--        d7s(7 downto  0) <= "11111001";
+--        d7s(15 downto 8) <= "10100100";
 
 Process(CLK100MHZ)
 begin
 if CLK100MHZ'event and CLK100MHZ = '1' and SW1 = '1' then
-
 -- Inicializacion de Juego
     if B1 = '1' and SON = '0' then
         SON <= '1';
         Int1 <= 0;
         Int2 <= 0;
-        Int3 <= 0;
-        Int4 <= 0;
+--        Int3 <= 0;
+--        Int4 <= 0;
     --elsif B1 = '1' and SON = '1' then
         --SON <= '0';
         --IntTJ1 <= 0;
@@ -165,13 +167,13 @@ if CLK100MHZ'event and CLK100MHZ = '1' and SW1 = '1' then
     end if;
     
     --Inicia Timer Juego
-    if SW1 = '1' and SON = '1' then
+    if SON = '1' then
         
-       Int1 <= Int3;
-       Int2 <= Int4;
        --Convertir ints a std_logic_vector
        SP1 <= conv_std_logic_vector(Int1, 4);
        SP2 <= conv_std_logic_vector(Int2, 4);
+--       Int1 <= Int3;
+--       Int2 <= Int4;
        T1 <= SP1;
        T2 <= SP2;
        
@@ -195,6 +197,7 @@ if CLK100MHZ'event and CLK100MHZ = '1' and SW1 = '1' then
             STJ1 <= "0000";
             STJ2 <= "0000";
             IntTJ2 <= IntTJ2 - 1;
+            --Int5 <= Int2;
             SON <= '0';
             SLCDE <= '1';
         --Disminuir valor del reloj
@@ -210,60 +213,71 @@ if CLK100MHZ'event and CLK100MHZ = '1' and SW1 = '1' then
         end if;
 
 -- Puntos
-    
+    NumLED(4 downto 0) <= std_logic_vector(to_unsigned(intTO, 5)); 
     --Bloquear botones por 1 segundo
---    if B2 = '1' and IntTO = 0 and IntTime = 0 then
---        IntTO <= 1;
---        STO <= '0';
---    --Bloquear botones por 7 segundos
---    elsif B3 = '1' and IntTO = 0  and IntTime = 0 then
---        IntTO <= 7;
---        STO <= '0';
---    --Bloquear botones por 15 segundos
---    elsif B4 = '1' and IntTO = 0 and IntTime = 0 then
---        IntTO <= 15;
---        STO <= '0';
---    --Disminuir reloj de bloqueo un segundo
---    elsif IntTO /= 0  and IntTime = 0 then
---        IntTO <= IntTO - 1;
---        STO <= '1';
---    --Volver a habilitar los botones
---    elsif B2 = '0' and B3 = '0' and B4 = '0' and IntTO = 0 and IntTime = 0 then
---        IntTO <= 0;
---        STO <= '1';
---    end if;
+    if B2 = '1' and B3 = '0' and B4 = '0' and intTO = 0 then
+        IntTO <= 1;
+        if Int1 = 9 then
+            Int1 <= 0;
+            Int2 <= Int2 + 1;
+        else
+            Int1 <= Int1 + 1;
+            Int2 <= Int2;
+        end if;
+    --Bloquear botones por 7 segundos
+    elsif B2 = '0' and B3 = '1' and B4 = '0' and IntTo = 0 then
+        IntTO <= 7;
+        if Int1 >= 5 then
+            Int1 <= Int1 - 5;
+            Int2 <= Int2 + 1;
+        else
+            Int1 <= Int1 + 5;
+            Int2 <= Int2;
+        end if;
+    --Bloquear botones por 15 segundos
+    elsif B2 = '0' and B3 = '0' and B4 = '1' and IntTo = 0 then
+        IntTO <= 15;
+        Int1 <= Int1;
+        Int2 <= Int2 + 1;
+    --Disminuir reloj de bloqueo un segundo
+    elsif IntTo /= 0  and IntTime = 0 then
+        IntTO <= IntTO - 1;
+    --Volver a habilitar los botones
+    elsif (B2 /= '1' or B3 /= '1' or B4 /= '1') and IntTO = 0 and IntTime = 0 then
+        IntTO <= 0;
+    end if;
 
-      if (Boton = 1 and Qout = '1') then
-          IntTo <= 1;
-          STO <= '0';
-      elsif (Boton = 2 and Qout = '1') then
-          IntTo <= 7;
-          STO <= '0';
-      elsif (Boton = 3 and Qout = '1') then
-          IntTo <= 15;
-          STO <= '0';
-      elsif (Boton = 0 and Qout = '0' and intTime = 0 and IntTo /= 0) then
-          IntTo <= IntTo - 1;
-          STO <= '0';
-      elsif (intTime = 0 and IntTo = 0) then
-          STO <= '1';
-      elsif (intTime = 0 and IntTo /= 0) then
-          STO <= '0';    
-      end if;
+--      if (Boton = 1) then
+--          IntTo <= 1;
+--          STO <= '0';
+--      elsif (Boton = 2) then
+--          IntTo <= 7;
+--          STO <= '0';
+--      elsif (Boton = 3) then
+--          IntTo <= 15;
+--          STO <= '0';
+--      elsif (intTime = 0 and IntTo /= 0) then
+--          IntTo <= IntTo - 1;
+--          STO <= '0';
+--      elsif (intTime = 0 and IntTo = 0) then
+--          STO <= '1';
+--      elsif (intTime = 0 and IntTo /= 0) then
+--          STO <= '0';    
+--      end if;
     
 -- Displays de 7 segmentos
     
-    --Multiplexar entre valores del timer
-    if SMUX = "10" then
-        D71 <= STJ71;
-        SMUX <= "01";
-    else
-        D71 <= STJ72;
-        SMUX <= "10";
-    end if;
+--    --Multiplexar entre valores del timer
+--    if SMUX = "10" then
+--        D71 <= STJ71;
+--        SMUX <= "00000001";
+--    else
+--        D71 <= STJ72;
+--        SMUX <= "00000010";
+--    end if;
     
-    --Pasar el valor a su salida
-        D7A <= SMUX;
+--    --Pasar el valor a su salida
+--        D7A <= SMUX;
     
     end if; 
     
@@ -271,15 +285,46 @@ if CLK100MHZ'event and CLK100MHZ = '1' and SW1 = '1' then
     STJ1 <= conv_std_logic_vector(IntTJ1, 4);
     STJ2 <= conv_std_logic_vector(IntTJ2, 4);
     
+    case (STJ1) is
+                        when "0000" => d7s(15 downto 8) <= "11000000";
+                        when "0001" => d7s(15 downto 8) <= "11111001";
+                        when "0010" => d7s(15 downto 8) <= "10100100";
+                        when "0011" => d7s(15 downto 8) <= "10110000";
+                        when "0100" => d7s(15 downto 8) <= "10011001";
+                        when "0101" => d7s(15 downto 8) <= "10010010";
+                        when "0110" => d7s(15 downto 8) <= "10000010";
+                        when "0111" => d7s(15 downto 8) <= "11111000";
+                        when "1000" => d7s(15 downto 8) <= "10000000";
+                        when "1001" => d7s(15 downto 8) <= "10010000";
+                        when others => d7s(15 downto 8) <= "11111111";
+                        end case;
+    
+    case (STJ2) is
+                        when "0000" => d7s(7 downto 0) <= "11000000";
+                        when "0001" => d7s(7 downto 0) <= "11111001";
+                        when "0010" => d7s(7 downto 0) <= "10100100";
+                        when "0011" => d7s(7 downto 0) <= "10110000";
+                        when "0100" => d7s(7 downto 0) <= "10011001";
+                        when "0101" => d7s(7 downto 0) <= "10010010";
+                        when "0110" => d7s(7 downto 0) <= "10000010";
+                        when "0111" => d7s(7 downto 0) <= "11111000";
+                        when "1000" => d7s(7 downto 0) <= "10000000";
+                        when "1001" => d7s(7 downto 0) <= "10010000";
+                        when others => d7s(7 downto 0) <= "11111111";
+                        end case;
+    
+    
 --PWM
     
     --Intpuls para abrir o cerrar puerta con servo cambia entre -90 y 90 grados
-    if (SON = '0' and Int2 <= Int5) then 
+    if (STO = '0') then
+        IntPuls <= 150000;
+    elsif (SON = '0' and Int5 <= Int2) then 
         IntPuls <= 250000;
     elsif (SON = '0' and Int2 > Int5 ) then
         IntPuls <= 50000;
-    else 
-        IntPuls <= 250000;
+--    else 
+--        IntPuls <= 250000;
     end if;
     
     --Prende salida cuando comp es menor a puls
@@ -291,6 +336,7 @@ if CLK100MHZ'event and CLK100MHZ = '1' and SW1 = '1' then
         SM1 <= '0';
         IntComp <= 0;
         Int5 <= Int5 + 1;
+        STO <= '1';
     --Apaga salida cuando comp es mayor a puls
     else 
         SM1 <= '0';
--- segm7.vhd
+++ segm7.vhd
@@ -19,59 +19,149 @@
 ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+--library IEEE;
+--use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+---- Uncomment the following library declaration if using
+---- arithmetic functions with Signed or Unsigned values
+----use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx leaf cells in this code.
+----library UNISIM;
+----use UNISIM.VComponents.all;
+
+--entity segm7 is
+--Port(
+--        Clk: in std_logic;
+--        N1, N2: in std_logic_vector(3 downto 0);
+--        S81, S82: out std_logic_vector(7 downto 0)
+--);
+--end segm7;
+
+--architecture Behavioral of segm7 is
+
+--begin
+--Process(Clk)
+--begin
+---- pasa valor de 4 bits a su equivalente para display de 7 segmentos
+--case (n2) is
+--                    when "0000" => S82 <= "11000000";
+--                    when "0001" => S82 <= "11111001";
+--                    when "0010" => S82 <= "10100100";
+--                    when "0011" => S82 <= "10110000";
+--                    when "0100" => S82 <= "10011001";
+--                    when "0101" => S82 <= "10010010";
+--                    when "0110" => S82 <= "10000010";
+--                    when "0111" => S82 <= "11111000";
+--                    when "1000" => S82 <= "10000000";
+--                    when "1001" => S82 <= "10010000";
+--                    when others => S82 <= "11111111";
+--                    end case;
+
+--case (n1) is
+--                    when "0000" => S81 <= "11000000";
+--                    when "0001" => S81 <= "11111001";
+--                    when "0010" => S81 <= "10100100";
+--                    when "0011" => S81 <= "10110000";
+--                    when "0100" => S81 <= "10011001";
+--                    when "0101" => S81 <= "10010010";
+--                    when "0110" => S81 <= "10000010";
+--                    when "0111" => S81 <= "11111000";
+--                    when "1000" => S81 <= "10000000";
+--                    when "1001" => S81 <= "10010000";
+--                    when others => S81 <= "11111111";
+--                    end case;
+
+--end process;
+--end Behavioral;
+
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------
+-- Author:  Mircea Dabacan
+--          Copyright 2014 Digilent, Inc.
+----------------------------------------------------------------------------
+-- 
+-- Create Date:    13:13:49 12/16/2010 
+-- Design Name: 
+-- Module Name:    sSegDisplay - Behavioral 
+-- Description: 
+--    This module represents the seven-segment display multiplexer
+--    Because the pattern to be displayed does not contain numerical or
+--    alphabetical characters representable on a seven-segment display,
+--    the incoming data is NOT encoded to seven-segment code, 
+--    instead the incoming data is sent directly to the cathodes, 
+--    according to the diagram shown below
+-- Segment encoding
+--      0
+--     ---  
+--  5 |   | 1
+--     ---   <- 6
+--  4 |   | 2
+--     ---
+--      3
+--  Decimal Point = 7
+--
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use IEEE.std_logic_unsigned.all;	-- add to do arithmetic operations
+use IEEE.std_logic_arith.all;		-- add to do arithmetic operations
 
 entity segm7 is
-Port(
-        Clk: in std_logic;
-        N1, N2: in std_logic_vector(3 downto 0);
-        S81, S82: out std_logic_vector(7 downto 0)
-);
+    Port(ck : in  std_logic;                          -- 100MHz system clock
+			number : in  std_logic_vector (63 downto 0); -- eight digit number to be displayed
+			seg : out  std_logic_vector (7 downto 0);    -- display cathodes
+			an : out  std_logic_vector (7 downto 0));    -- display anodes (active-low, due to transistor complementing)
 end segm7;
 
 architecture Behavioral of segm7 is
 
-begin
-Process(Clk)
-begin
--- pasa valor de 4 bits a su equivalente para display de 7 segmentos
-case (n2) is
-                    when "0000" => S82 <= "11000000";
-                    when "0001" => S82 <= "11111001";
-                    when "0010" => S82 <= "10100100";
-                    when "0011" => S82 <= "10110000";
-                    when "0100" => S82 <= "10011001";
-                    when "0101" => S82 <= "10010010";
-                    when "0110" => S82 <= "10000010";
-                    when "0111" => S82 <= "11111000";
-                    when "1000" => S82 <= "10000000";
-                    when "1001" => S82 <= "10010000";
-                    when others => S82 <= "11111111";
-                    end case;
+	signal cnt: std_logic_vector(19 downto 0); -- divider counter for ~95.3Hz refresh rate (with 100MHz main clock)
+	signal hex: std_logic_vector(7 downto 0);  -- hexadecimal digit
+	signal intAn: std_logic_vector(7 downto 0); -- internal signal representing anode data
 
-case (n1) is
-                    when "0000" => S81 <= "11000000";
-                    when "0001" => S81 <= "11111001";
-                    when "0010" => S81 <= "10100100";
-                    when "0011" => S81 <= "10110000";
-                    when "0100" => S81 <= "10011001";
-                    when "0101" => S81 <= "10010010";
-                    when "0110" => S81 <= "10000010";
-                    when "0111" => S81 <= "11111000";
-                    when "1000" => S81 <= "10000000";
-                    when "1001" => S81 <= "10010000";
-                    when others => S81 <= "11111111";
-                    end case;
+begin
 
-end process;
+   -- Assign outputs
+   an <= intAn;
+   seg <= hex;
+
+   clockDivider: process(ck)
+   begin
+      if ck'event and ck = '1' then
+         cnt <= cnt + '1';
+      end if;
+   end process clockDivider;
+
+   -- Anode Select
+   with cnt(19 downto 17) select -- 100MHz/2^20 = 95.3Hz
+      intAn <=    
+         "11111110" when "000", -- Display de la derecha
+         "11111101" when "001",
+         "11111011" when "010",
+         "11110111" when "011",
+         "11101111" when "100",
+         "11011111" when "101",
+         "10111111" when "110",
+         "01111111" when others; -- Display de la izquierda
+
+   -- Digit Select
+   with cnt(19 downto 17) select -- 100MHz/2^20 = 95.3Hz
+      hex <=      
+         number(7 downto 0)   when "000",
+         number(15 downto 8)  when "001",
+         number(23 downto 16) when "010",
+         number(31 downto 24) when "011",
+         number(39 downto 32) when "100",
+         number(47 downto 40) when "101",
+         number(55 downto 48) when "110",
+         number(63 downto 56) when others;
+
 end Behavioral;
